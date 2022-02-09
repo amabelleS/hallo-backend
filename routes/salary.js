cc656bb568755b981135b3667f5f779d640a4b70
amabelle <amabelletr@gmail.com>
2022-02-10 01:19:46 +0200

    GET popularity

--- routes/salary.js
+++ routes/salary.js
@@ -33,7 +33,21 @@ router.get('/avg', async function (req, res) {
     res.status(400).send(error.message);
   }
 
-  res.status(200).json({ job: req.params.job });
+  //   res.status(200).json({ job: req.params.job });
+});
+
+router.get('/popularity', async function (req, res) {
+  try {
+    // const sqlQuery = 'SELECT name, job, salary FROM ppl_salary WHERE job=?';
+    const sqlQuery = `SELECT job, COUNT(job) AS popularity FROM ppl_salary GROUP BY job`;
+    const rows = await db.pool.query(sqlQuery, req.params.job);
+
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+
+  //   res.status(200).json({ job: req.params.job });
 });
 
 router.get('/:job', async function (req, res) {
